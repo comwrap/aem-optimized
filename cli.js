74c9ac17cca88459eb0d94e0ebac452aae6a873b
Helge Ahrens <helgeahrens@Helges-MacBook-Pro.local>
2022-04-10 18:53:41 +0200

    new api. Now uses vite.config.js to laod data

--- cli.js
+++ cli.js
@@ -1,25 +1,51 @@
 #!/usr/bin/env node
-
-import { cosmiconfig } from 'cosmiconfig';
-import { createServer } from './server.js';
-const explorer = cosmiconfig('aemoptimized');
-explorer
-  .search()
-  .then((result) => {
-    console.log(result.config);
-    // result.config is the parsed configuration object.
-    // result.filepath is the path to the config file that was found.
-    // result.isEmpty is true if there was nothing to parse in the config file.
-    createServer(
-      result.config.host,
-      result.config.clientlibs,
-      result.config.port,
-      result.config.entry,
-      result.config.headers
-    );
+// import fs from 'fs';
+const fs = require('fs');
+const path = require('path');
+const esbuild = require('esbuild');
+const currPath = process.cwd();
+// import { createServer } from './server.js'
+const createServer = require('./dist/server').default;
+// import esbuild from 'esbuild';
+// import { cosmiconfig } from 'cosmiconfig';
+// import { createServer } from './server.js';
+// const explorer = cosmiconfig('aemoptimized');
+// explorer
+//   .search()
+//   .then((result) => {
+//     console.log(result.config);
+//     // result.config is the parsed configuration object.
+//     // result.filepath is the path to the config file that was found.
+//     // result.isEmpty is true if there was nothing to parse in the config file.
+//     createServer(
+//       result.config.host,
+//       result.config.clientlibs,
+//       result.config.port,
+//       result.config.entry,
+//       result.config.headers
+//     );
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//     // Do something constructive.
+//   });
+const pathToConfig = process.cwd() + '/vite.config.js';
+const processedConfig = path.join(process.cwd(), 'esbuild', 'vite.config.js');
+esbuild
+  .build({
+    format: 'cjs',
+    entryPoints: [pathToConfig],
+    outdir: './esbuild',
   })
-  .catch((error) => {
-    console.error(error);
-    // Do something constructive.
-  });
-// const loaded = explorerSync.load(pathToConfig);
+  .catch((e) => console.log(e));
+if (fs.existsSync(processedConfig)) {
+  const config = require(processedConfig).default;
+  console.log(config.aemOptimized);
+  createServer(
+    config.aemOptimized.host,
+    config.aemOptimized.clientlibs,
+    config.aemOptimized.port,
+    config.aemOptimized.entry,
+    config.aemOptimized.headers
+  );
+}
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import { request } from 'express';
-import { JSDOM } from 'jsdom';
-import fetch from 'node-fetch';
-import vite from 'vite';
+const express = require('express');
+const jsdom = require('jsdom');
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const vite = require('vite');
 
-const { createServer: createViteServer } = vite;
+const { createServer } = vite;
+const { JSDOM } = jsdom;
+
 async function init(url, host, clientlib, entry, headers) {
   // fetch with basic auth admin:admin
   let page;
@@ -51,7 +53,7 @@ async function init(url, host, clientlib, entry, headers) {
   return dom.serialize();
 }
 
-async function createServer(host, clientlibs, port, entry, headers) {
+async function createMyServer(host, clientlibs, port, entry, headers) {
   const app = express();
 
   // Create Vite server in middleware mode. This disables Vite's own HTML
@@ -59,7 +61,7 @@ async function createServer(host, clientlibs, port, entry, headers) {
   //
   // In middleware mode, if you want to use Vite's own HTML serving logic
   // use `'html'` as the `middlewareMode` (ref https://vitejs.dev/config/#server-middlewaremode)
-  const vite = await createViteServer({
+  const vite = await createServer({
     server: { middlewareMode: 'ssr' },
   });
   // use vite's connect instance as middleware
@@ -80,5 +82,5 @@ async function createServer(host, clientlibs, port, entry, headers) {
     console.log(`Server listening http://localhost:${port}`);
   });
 }
-
-export { createServer };
+// const createMyServer = () => {};
+export default createMyServer;

3927989d72d143c6b1a30f9768c72899a55b513d
Helge <elitaer@gmail.com>
2022-04-24 12:50:33 +0200

    fix async loading of processed config

--- cli.js
+++ cli.js
@@ -1,34 +1,9 @@
 #!/usr/bin/env node
-// import fs from 'fs';
 const fs = require('fs');
 const path = require('path');
 const esbuild = require('esbuild');
-const currPath = process.cwd();
-// import { createServer } from './server.js'
 const createServer = require('./dist/server').default;
-// import esbuild from 'esbuild';
-// import { cosmiconfig } from 'cosmiconfig';
-// import { createServer } from './server.js';
-// const explorer = cosmiconfig('aemoptimized');
-// explorer
-//   .search()
-//   .then((result) => {
-//     console.log(result.config);
-//     // result.config is the parsed configuration object.
-//     // result.filepath is the path to the config file that was found.
-//     // result.isEmpty is true if there was nothing to parse in the config file.
-//     createServer(
-//       result.config.host,
-//       result.config.clientlibs,
-//       result.config.port,
-//       result.config.entry,
-//       result.config.headers
-//     );
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//     // Do something constructive.
-//   });
+
 const pathToConfig = process.cwd() + '/vite.config.js';
 const processedConfig = path.join(
   process.cwd(),
@@ -36,22 +11,25 @@ const processedConfig = path.join(
   '.aemoptimized',
   'vite.config.js'
 );
+
 esbuild
   .build({
     format: 'cjs',
     entryPoints: [pathToConfig],
     outdir: './node_modules/.aemoptimized',
   })
+  .then(() => {
+    if (fs.existsSync(processedConfig)) {
+      const config = require(processedConfig).default;
+      console.log(config.aemOptimized);
+      createServer(
+        config.aemOptimized.host,
+        config.aemOptimized.clientlibs,
+        config.aemOptimized.port,
+        config.aemOptimized.entry,
+        config.aemOptimized.headers,
+        config.aemOptimized.wcmmode
+      );
+    }
+  })
   .catch((e) => console.log(e));
-if (fs.existsSync(processedConfig)) {
-  const config = require(processedConfig).default;
-  console.log(config.aemOptimized);
-  createServer(
-    config.aemOptimized.host,
-    config.aemOptimized.clientlibs,
-    config.aemOptimized.port,
-    config.aemOptimized.entry,
-    config.aemOptimized.headers,
-    config.aemOptimized.wcmmode
-  );
-}
